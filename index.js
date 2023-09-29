88f3979e8b480d32ce6253fb5e787b22172ffccd
myatkoheinhmm <142959766+myatkoheinhmm@users.noreply.github.com>
2023-09-29 08:42:42 +0630

    day4 assignment

--- index.js
+++ index.js
@@ -6,52 +6,47 @@ const url = require('url');
 const hostname = '127.0.0.1';
 const port = 8000;
 
-const welcome = fs.readFileSync(path.join(__dirname,'template','welcome.html'),'utf-8');
-const products = fs.readFileSync(path.join(__dirname,'api','data.json'),'utf-8');
-const productList = fs.readFileSync(path.join(__dirname,'template','product-list.html'),'utf-8');
+const mainTemplate = fs.readFileSync(path.join(__dirname,'product','index.html'),'utf-8');
+const productCard = fs.readFileSync(path.join(__dirname,'product',"product-card.html"),'utf-8');
+const products = JSON.parse(fs.readFileSync(path.join(__dirname,'data','product.json'),'utf-8'));
 
-function fileRead(filePath,encode = 'utf-8'){
-    fs.readFileSync(filePath,encode,(err,data) => {
-        if(err) throw err;
-        console.log(data);
-    });
+function productCardTemplate(productCard,product){
+    let output = productCard.replace('{%PRODUCT_TITLE%}',product.name);
+    output = output.replace('{%PRODUCT_DECRIPTION%}',product.description)
+    output = output.replace('{%PRODUCT_PRICE%}',product.price)
+    output = output.replace('{%PRODUCT_IMAGE%}',product.image)
+    return output;
 }
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req,res) => {
     const pathName = req.url;
-    if(pathName === '/'){
+    if(pathName === '/' || pathName === '/products'){
         res.writeHead(200, {
-            'Content-Type': 'text/html',
+            'Content-type': 'text/html'
+        })
+        
+        const productCardList =  products.map((product) => productCardTemplate(productCard,product));
+        console.log(productCardList);
+        res.end(mainTemplate.replace('{%PRODUCT%}',productCardList.join('')));
+    }else if(pathName === '/cart'){
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        
+        res.end(mainTemplate.replace('{%PRODUCT%}','Cart List'));
+    }else if(pathName === '/about'){
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        res.end(mainTemplate.replace('{%PRODUCT%}','About US'));
+    }
+    else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
         });
-        res.end(welcome.replace('{{%BODY%}}','HOME PAGE'));
-    }else if(pathName === '/about'){ 
-        res.writeHead(200,{
-            'Content-Type' : 'text/html'
-        });
-        res.end(welcome.replace('{{%BODY%}}','About Page'));
-    }else if(pathName === '/contact'){ 
-        res.writeHead(200,{
-            'Content-Type' : 'text/html'
-        });
-        res.end(welcome.replace('{{%BODY%}}','Contact Page'));
-    }else if(pathName === '/products'){
-        res.writeHead(200,{
-            'Content-Type' : 'text/html'
-        });
-        res.end(welcome.replace('{{%BODY%}}',productList));
-    }else if(pathName === '/api'){
-        res.writeHead(200,{
-            'Content-Type' : 'application/json'
-        });
-        res.end(products);
-    }else{ // 404
-        res.writeHead(404,{
-            'Content': 'text/html'
-        });
-        res.end('<h1>Not Found</h1>');
+        res.end('<h1>Page not found</h1>');
     }
 });
-
 
 
 server.listen(port,hostname,()=>{
